7d80b7d961f89d22e8650b2607aee0ee4adbf88e
Thiago Cavalcanti <thiago.remigio100@hotmail.com>
2025-12-29 23:38:46 -0300

    feat: update home message

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 80; // porta que a aplicação vai escutar
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.end("Hello World! Aplicação Node.js rodando na porta 80 🚀");
+  res.end("Olá mundo! Aplicação Node.js rodando na porta 80 🚀");
 });
 
 server.listen(port, () => {
